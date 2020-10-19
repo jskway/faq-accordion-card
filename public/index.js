e69cca822717f016be9c179a3820245a1f22f5ac
jackskim <jackskim29@gmail.com>
2020-10-18 18:28:12 -0700

    Add arrow reverse functionality

--- public/index.js
+++ public/index.js
@@ -7,6 +7,9 @@ accordianList.addEventListener("click", (e) => {
   question.classList.toggle("active");
   answer.classList.toggle("hidden");
 
+  const arrow = question.children[1];
+  arrow.classList.toggle("reverse");
+
   [...accordianItems].forEach((item) => {
     if (item !== e.target) {
       const [itemQuestion, itemAnswer] = item.children;
@@ -14,6 +17,8 @@ accordianList.addEventListener("click", (e) => {
       itemQuestion.classList.remove("active");
 
       if (![...itemAnswer.classList].includes("hidden")) {
+        const itemArrow = itemQuestion.children[1];
+        itemArrow.classList.remove("reverse");
         itemAnswer.classList.add("hidden");
       }
     }
